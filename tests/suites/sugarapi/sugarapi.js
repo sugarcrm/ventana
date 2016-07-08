c074579c614efed4872136f4c578c4f20aa63daf
Boro Sitnikovski <bsitnikovski@sugarcrm.com>
2016-07-08 20:12:08 +0200

    [SC-5841]: Remove unused variables in Sidecar

--- tests/suites/sugarapi/sugarapi.js
+++ tests/suites/sugarapi/sugarapi.js
@@ -700,7 +700,7 @@ describe('SugarCRM Javascript API', function () {
             SugarTest.server.respondWith("GET", "/rest/v10/metadata?type_filter=&module_filter=Contacts&module_dependencies=1",
                 [200, {  "Content-Type":"application/json"},
                     JSON.stringify(fixtures.metadata.modules.Contacts)]);
-            this.api.getMetadata("hash", [], ['Contacts'], this.callbacks);
+            this.api.getMetadata([], ['Contacts'], this.callbacks);
             SugarTest.server.respond();
 
             expect(callspy).toHaveBeenCalled();
@@ -711,7 +711,7 @@ describe('SugarCRM Javascript API', function () {
         it('should handle options params', function () {
             var callstub = sinon.stub(this.api, 'call');
 
-            this.api.getMetadata("hash", [], ['Contacts'], this.callbacks, {params:{lang:"en_us"}});
+            this.api.getMetadata([], ['Contacts'], this.callbacks, {params:{lang:"en_us"}});
 
             expect(callstub).toHaveBeenCalled();
             expect(callstub.getCall(0).args[1]).toEqual("/rest/v10/metadata?lang=en_us&type_filter=&module_filter=Contacts&module_dependencies=1");
@@ -727,7 +727,7 @@ describe('SugarCRM Javascript API', function () {
                 [200, {  "Content-Type":"application/json"},
                     JSON.stringify(fixtures.metadata.modules.Contacts)]);
 
-            this.api.getMetadata("hash", types, modules, this.callbacks);
+            this.api.getMetadata(types, modules, this.callbacks);
             SugarTest.server.respond(); //tell server to respond to pending async call
 
             expect(spy).toHaveBeenCalled();
