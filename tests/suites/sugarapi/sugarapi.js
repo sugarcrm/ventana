1a2d4dc6c28088fa2710d201600e649b3bfe3e7c
Filipe Guerra <alias.mac@gmail.com>
2016-07-10 05:24:51 -0700

    Improve SUGAR.Api#getMetadata signature to use options (SC-5841)
    Restore api's getMetadata tests to prove they still work with new signature.
    This allows us to cherry pick to previous versions.

--- tests/suites/sugarapi/sugarapi.js
+++ tests/suites/sugarapi/sugarapi.js
@@ -36,6 +36,7 @@ describe('SugarCRM Javascript API', function () {
     });
 
     afterEach(function () {
+        sinon.collection.restore();
         if (this.callbacks.success.restore) this.callbacks.success.restore();
         if (this.callbacks.error.restore) this.callbacks.error.restore();
         if (this.callbacks.complete.restore) this.callbacks.complete.restore();
@@ -694,13 +695,26 @@ describe('SugarCRM Javascript API', function () {
 
     describe('Metadata actions', function () {
 
+        it('should log a deprecation warning if old signature is used', function() {
+            sinon.collection.stub(this.api, 'call');
+            var warnStub = sinon.collection.stub(console, 'warn');
+
+            this.api.getMetadata('hash', '', '', '', '');
+            expect(warnStub).toHaveBeenCalled();
+
+            warnStub.reset();
+
+            this.api.getMetadata({});
+            expect(warnStub).not.toHaveBeenCalled();
+        });
+
         it('should delegate to the call method', function () {
             var callspy = sinon.spy(this.api, 'call');
 
             SugarTest.server.respondWith("GET", "/rest/v10/metadata?type_filter=&module_filter=Contacts&module_dependencies=1",
                 [200, {  "Content-Type":"application/json"},
                     JSON.stringify(fixtures.metadata.modules.Contacts)]);
-            this.api.getMetadata([], ['Contacts'], this.callbacks);
+            this.api.getMetadata('hash', [], ['Contacts'], this.callbacks);
             SugarTest.server.respond();
 
             expect(callspy).toHaveBeenCalled();
@@ -711,7 +725,7 @@ describe('SugarCRM Javascript API', function () {
         it('should handle options params', function () {
             var callstub = sinon.stub(this.api, 'call');
 
-            this.api.getMetadata([], ['Contacts'], this.callbacks, {params:{lang:"en_us"}});
+            this.api.getMetadata('hash', [], ['Contacts'], this.callbacks, {params:{lang:"en_us"}});
 
             expect(callstub).toHaveBeenCalled();
             expect(callstub.getCall(0).args[1]).toEqual("/rest/v10/metadata?lang=en_us&type_filter=&module_filter=Contacts&module_dependencies=1");
@@ -727,7 +741,7 @@ describe('SugarCRM Javascript API', function () {
                 [200, {  "Content-Type":"application/json"},
                     JSON.stringify(fixtures.metadata.modules.Contacts)]);
 
-            this.api.getMetadata(types, modules, this.callbacks);
+            this.api.getMetadata('hash', types, modules, this.callbacks);
             SugarTest.server.respond(); //tell server to respond to pending async call
 
             expect(spy).toHaveBeenCalled();
