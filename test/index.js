b89f1ce561bca17d5a17403bc39a98e1ae1ab39d
Filipe Guerra <alias.mac@gmail.com>
2017-02-20 00:27:47 -0800

    Finally no more SugarTest global variable!

--- test/index.js
+++ test/index.js
@@ -1,22 +1,5 @@
 
 require('../src/client.js');
 
-var SugarTest = {};
-
-(function(test) {
-
-    test.waitFlag = false;
-    test.wait = function() { waitsFor(function() { return test.waitFlag; }); };
-    test.resetWaitFlag = function() { this.waitFlag = false; };
-    test.setWaitFlag = function() { this.waitFlag = true; };
-
-}(SugarTest));
-
-beforeEach(function(){
-    SugarTest.resetWaitFlag();
-});
-
-window.SugarTest = SugarTest;
-
 var testsContext = require.context('./', true, /client\.js/);
 testsContext.keys().forEach(testsContext);
