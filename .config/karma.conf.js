f50389141c8ff586376bddf39548565c6dc434d4
Filipe Guerra <alias.mac@gmail.com>
2017-02-20 22:49:46 -0800

    Add SauceLabs tests

--- .config/karma.conf.js
+++ .config/karma.conf.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 module.exports = function (config) {
 
-    let conf = {
+    config.set({
         basePath: '../',
 
         files: [
@@ -50,6 +50,10 @@ module.exports = function (config) {
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['Chrome'],
 
+        browserDisconnectTimeout: 10000,
+        browserDisconnectTolerance: 2,
+        browserNoActivityTimeout: 30000,
+
         webpack: {
             devtool: 'inline-source-map',
             module: {
@@ -90,13 +94,53 @@ module.exports = function (config) {
             Chrome_CI: {
                 base: 'Chrome',
                 flags: ['--no-sandbox'],
-            }
+            },
+            sl_chrome: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                version: 55,
+            },
+            sl_firefox: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+            },
+            sl_safari: {
+                base: 'SauceLabs',
+                browserName: 'safari',
+            },
+            sl_ie: {
+                base: 'SauceLabs',
+                browserName: 'internet explorer',
+            },
+            sl_edge: {
+                base: 'SauceLabs',
+                browserName: 'MicrosoftEdge',
+            },
         },
-    };
+
+        sauceLabs: {
+            testName: 'Ventana Karma Tests',
+        },
+    });
 
     if (process.env.TRAVIS) {
-        conf.browsers = ['Chrome_CI'];
+
+        let buildLabel = `TRAVIS #${process.env.TRAVIS_BUILD_NUMBER} (${process.env.TRAVIS_BUILD_ID})`;
+
+        config.sauceLabs.build = buildLabel;
+        config.sauceLabs.startConnect = false;
+        config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+
+        // Increase timeout in case connection in CI is slow
+        config.browserNoActivityTimeout = 120000;
+
+        config.reporters = ['dots', 'coverage', 'saucelabs'];
+
+        // Allocating a browser can take pretty long (eg. if we are out of
+        // capacity and need to wait for another build to finish) and so the
+        // `captureTimeout` typically kills an in-queue-pending request, which
+        // makes no sense.
+        config.captureTimeout = 0;
     }
 
-    config.set(conf);
 };
--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Ventana [![Build Status](https://travis-ci.org/sugarcrm/ventana.svg?branch=master)](https://travis-ci.org/sugarcrm/ventana) [![Coverage Status](https://coveralls.io/repos/github/sugarcrm/ventana/badge.svg?branch=master)](https://coveralls.io/github/sugarcrm/ventana?branch=master)
+# Ventana [![Build Status](https://travis-ci.org/sugarcrm/ventana.svg?branch=master)](https://travis-ci.org/sugarcrm/ventana) [![Coverage Status](https://coveralls.io/repos/github/sugarcrm/ventana/badge.svg?branch=master)](https://coveralls.io/github/sugarcrm/ventana?branch=master) [![Sauce Test Status](https://saucelabs.com/buildstatus/hackers-sugarcrm)](https://saucelabs.com/u/hackers-sugarcrm)
 
 Ventana is a client to help connecting and making requests to a SugarCRM REST
 API on multiple versions.
 
 Currently this library provides only a Javascript connector. Please contribute
 to help us grow this client connector.
+
+[![Sauce Test Status](https://saucelabs.com/browser-matrix/hackers-sugarcrm.svg)](https://saucelabs.com/u/hackers-sugarcrm)
 
 ## Contributing
 
