f7875b78a5fb3b6dda3812cb5669582412287040
apopa-sugarcrm <adrian.popa@sugarcrm.com>
2024-10-18 11:22:26 +0300

    CA-2837: eslint upgrade

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+const babelParser = require('@babel/eslint-parser');
+const importPlugin = require('eslint-plugin-import');
+
+module.exports = [
+  {
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+      },
+      globals: {
+        _: 'readonly',
+        sinon: 'readonly',
+        browser: true,
+        jasmine: true,
+        jquery: true,
+      },
+    },
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+        },
+      ],
+      'import/prefer-default-export': 'off',
+      'no-console': 'warn',
+      'semi': ['error', 'always'],
+      'indent': [
+        'error',
+        4,
+        {
+          'SwitchCase': 1,
+        },
+      ],
+      'no-unused-vars': [
+        'error',
+        {
+          'args': 'none',
+        },
+      ],
+      'comma-dangle': [
+        'error',
+        {
+          'arrays': 'always-multiline',
+          'objects': 'always-multiline',
+        },
+      ],
+      'global-require': 'off',
+      'max-len': [
+        'error',
+        120,
+      ],
+      'no-underscore-dangle': 'off',
+      'space-before-function-paren': [
+        'error',
+        'never',
+      ],
+      'no-plusplus': 'off',
+      'no-restricted-properties': 'off',
+      'object-curly-spacing': [
+        'error',
+        'never',
+      ],
+    },
+  },
+];
